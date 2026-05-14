78a6dd3f60f7e44ee15b98b610652f5a8bcf4a01
admin <m4rinacotowa@yandex.ru>
2026-05-14 10:53:31 +0500

    update

--- forms/profile_setup.py
+++ forms/profile_setup.py
@@ -9,18 +9,18 @@ class ProfileSetupForm(FlaskForm):
                        validators=[
                            FileAllowed(['jpg', 'png', 'jpeg'], message='Недопустимый формат файла')
                        ])
-    username = StringField('Имя пользователя (обязательно)',
+    username = StringField('Имя пользователя',
                            validators=[
                                DataRequired(message='Поле должно быть заполнено'),
-                               Length(min=5, max=20, message='Имя пользователя должно содержать от 5 до 20 символов'),
+                               Length(min=4, max=20, message='Имя пользователя должно содержать от 5 до 20 символов'),
                                Regexp(regex=r'^[a-zA-Z0-9_]+$',
                                       message='Имя пользователя может содержать только латинские буквы, цифры и нижнее подчеркивание')
                            ])
-    nickname = StringField('Отображаемое имя (обязательно)',
+    nickname = StringField('Отображаемое имя',
                            validators=[
                                DataRequired(message='Поле должно быть заполнено'),
                                Length(min=1, max=30, message='Имя должно содержать от 1 до 30 символов')
                            ])
-    description = StringField('О себе (необязательно)',
+    description = StringField('О себе',
                               validators=[Length(max=150, message='Описание не должно быть длиннее 150 символов')])
     submit = SubmitField('Создать аккаунт')
